b3df4e8fa981159fb36dca843453db51d2fadecc
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-06-07 19:06:25 +0530

    changed 5 files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import NotFound from "./pages/404/NotFound.jsx";
 import Home from "./pages/home/Home.jsx";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
 
       <Footer></Footer>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,38 @@
-function Header() {
-  return <div>Header</div>;
-}
+import { useState } from "react";
+import styles from "./header.module.scss";
+
+const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className={styles.header}>
+      <div className={styles.logo}>
+        React<span>Starter</span>.
+      </div>
+      <div
+        className={`${styles["nav-links"]} ${
+          isMobileMenuOpen ? styles.show : ""
+        }`}
+      >
+        <a href="#home" className="active">
+          Home
+        </a>
+        <a href="#dashboard">Dashboard</a>
+      </div>
+      <div className={styles["auth-buttons"]}>
+        <a href="#login">Login</a>
+        <a href="#register">Register</a>
+        <button className={styles["get-started"]}>Get Started</button>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMobileMenu}>
+        &#9776;
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
