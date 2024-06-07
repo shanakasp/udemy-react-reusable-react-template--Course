3e0a9aa602f5c6e69b7e3b5f1ab9d1722f11366f
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-06-07 09:38:47 +0530

    changed 12 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Datafetch from "./Datafetch";
-
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home.jsx";
 function App() {
   return (
-    <>
-      <div>
-        <Datafetch></Datafetch>
-      </div>
-    </>
+    <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+
+      <Footer></Footer>
+    </div>
   );
 }
 
