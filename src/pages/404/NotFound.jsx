a347a31b034ab8138235b1d16dc098d17697474c
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-06-08 11:01:14 +0530

    changed 5 files

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./notfound.module.scss";
+
 const NotFound = () => {
-  return <div>Not Found</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className={styles["not-found-container"]}>
+      {isLoading ? (
+        <div className={styles["loading-circle"]} />
+      ) : (
+        <div className={styles["not-found-content"]}>
+          <div className="icon-container"></div>
+          <h2>Oops! Page Not Found</h2>
+          <p>Sorry, but the page you are looking for does not exist.</p>
+          <Link to={"/"}>
+            <button>Back to Home Page</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default NotFound;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,32 @@
+import styles from "./home.module.scss";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.body}>
+      <div className={styles.hero}>
+        <img
+          src="https://via.placeholder.com/1500x500"
+          alt="Hero"
+          className={styles.heroImage}
+        />
+        <div className={styles.heroText}>
+          <h1>Welcome to HomePage</h1>
+          <p>Your journey to Home Page!</p>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h2>About Us</h2>
+        <p>Hikz</p>
+        <h2>Features</h2>
+        <ul>
+          <li>Easy to follow friends</li>
+          <li>Real-world profile</li>
+          <li>Community help</li>
+          <li>Regular products</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
